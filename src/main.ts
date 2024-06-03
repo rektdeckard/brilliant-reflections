b9c69edc4253e79c24a8e135f6bc03c3b88e2465
rektdeckard <friedtm@gmail.com>
2024-06-02 22:27:36 -0600

    chore(app): remove unused imports

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,6 @@
 import p5 from "p5";
-// import v1 from "./interactives/v1";
-// import v2 from "./interactives/v2";
-// import v3 from "./interactives/v3";
-// import v4 from "./interactives/v4";
-// import v5 from "./interactives/v5";
-// import v6 from "./interactives/v6";
-// import v7 from "./interactives/v7";
 import v8 from "./interactives/v8";
 import "./style.css"
 
 new p5(v8, document.querySelector<HTMLElement>("#interactive")!);
-// new p5(v7, document.querySelector<HTMLElement>("main")!);
-// new p5(v6, document.querySelector<HTMLElement>("main")!);
-// new p5(v5, document.querySelector<HTMLElement>("main")!);
-// new p5(v4, document.querySelector<HTMLElement>("main")!);
-// new p5(v3, document.querySelector<HTMLElement>("main")!);
-// new p5(v2, document.querySelector<HTMLElement>("main")!);
-// new p5(v1, document.querySelector<HTMLElement>("main")!);
 
